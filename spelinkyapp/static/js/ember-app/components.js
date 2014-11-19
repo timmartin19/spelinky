1e00cbc74137d607fff56a42c74ae06920d63d18
Tim Martin <tbm19@case.edu>
2014-11-18 23:27:19 -0500

    So MANY COMPONENTS
    I may have a problem.  Long story short things with components now:
    Links, Indvidual link, comments, individual comment, short-hand profile
    image and name/link.

--- spelinkyapp/static/js/ember-app/components.js
+++ spelinkyapp/static/js/ember-app/components.js
@@ -22,3 +22,29 @@ Spelinky.LinksContainerComponent = Ember.Component.extend({
         }
     }
 });
+
+Spelinky.CommentsContainerComponent = Ember.Component.extend({
+    actions: {
+        toggleComments: function(){
+            this.set('viewingComments', !this.get('viewingComments'));
+        },
+        postComment: function(comment){
+            if(!comment){return false;}
+            if(!comment.trim()){return;}
+            var store = this.get('store');
+            var link = this.get('model');
+            var self = this;
+            var userId = $('meta#userId').attr('content');
+            store.find('user', userId).then(function(owner){
+                var new_comment = store.createRecord('comment', {
+                    comment_text: comment,
+                    owner: owner,
+                    link: link
+                });
+                new_comment.save();
+                self.set('newComment', '');
+            })
+        }
+    },
+    viewingComments: false
+});
--- spelinkyapp/static/js/ember-app/controllers.js
+++ spelinkyapp/static/js/ember-app/controllers.js
@@ -3,48 +3,6 @@
  */
 
 Spelinky.HomeIndexController = Ember.ArrayController.extend({
-    itemController: 'link',
     sortProperties: ['date_uploaded'],
     sortAscending: true
-});
-
-Spelinky.LinkController = Ember.ObjectController.extend({
-    actions: {
-        toggleComments: function(){
-            this.set('viewingComments', !this.get('viewingComments'));
-        },
-        postComment: function(comment){
-            if(!comment){return false;}
-            if(!comment.trim()){return;}
-            var store = this.get('store');
-            var link = this.get('model');
-            var self = this;
-            var userId = $('meta#userId').attr('content');
-            store.find('user', userId).then(function(owner){
-                var new_comment = store.createRecord('comment', {
-                    comment_text: comment,
-                    owner: owner,
-                    link: link
-                });
-                new_comment.save();
-                self.set('newComment', '');
-            })
-        }
-    },
-    viewingComments: false
-});
-
-Spelinky.ProfileController = Ember.ObjectController.extend({
-    needs: ['home_index', 'link'],
-    actions: {
-        postLink: function(url){
-            this.get('controllers.home_index').send('postLink', url);
-        },
-        toggleComments: function(){
-            this.get('controllers.link').send('toggleComments');
-        },
-        postComment: function(comment){
-            this.get('controllers.link').send('postComment', comment);
-        }
-    }
 });
